ec03b7d9c9aaa5197369aec83db0723fec8d6857
Matt Fisher <m@ttfisher.com>
2023-06-30 18:06:21 +1000

    Add some test views

--- example_project/test_app/urls.py
+++ example_project/test_app/urls.py
@@ -1,1 +1,40 @@
-urlpatterns = []
+from django.urls import (
+    include,
+    path,
+)
+
+from . import views
+
+app_name = 'test_app'
+urlpatterns = [
+    path(
+        'public/',
+        include(
+            [
+                path('basic', views.basic_public_view, name='basic_public'),
+                path(
+                    'basic_fixture/<int:pk>',
+                    views.basic_public_view_requiring_fixture,
+                    name='basic_public_fixture',
+                ),
+            ],
+        ),
+    ),
+    path(
+        'private/',
+        include(
+            [
+                path(
+                    'basic_redirects',
+                    views.basic_private_view_that_redirects,
+                    name='basic_private_redirects',
+                ),
+                path(
+                    'basic_403',
+                    views.basic_private_view_that_raises_403,
+                    name='basic_private_view_that_raises_403',
+                ),
+            ],
+        ),
+    ),
+]
--- example_project/test_app/views.py
+++ example_project/test_app/views.py
@@ -1,1 +1,29 @@
-# Create your views here.
+from django.contrib.auth.decorators import (
+    login_required,
+    permission_required,
+)
+from django.http import HttpResponse
+from django.shortcuts import (
+    get_object_or_404,
+    render,
+)
+from polls.models import Question
+
+
+def basic_public_view(request):
+    return HttpResponse('<html><body>Public</body></html')
+
+
+@login_required
+def basic_private_view_that_redirects(request):
+    return HttpResponse('<html><body>Private</body></html')
+
+
+@permission_required('polls.add_question', raise_exception=True)
+def basic_private_view_that_raises_403(request):
+    return HttpResponse('<html><body>Private</body></html')
+
+
+def basic_public_view_requiring_fixture(request, *, pk):
+    question = get_object_or_404(Question, pk=pk)
+    return render(request, 'polls/detail.html', {'question': question})
