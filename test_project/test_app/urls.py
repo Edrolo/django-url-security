1a89421b68d443f2a3e69d83627ccf335d4830e1
Matt Fisher <m@ttfisher.com>
2023-06-30 17:31:44 +1000

    Move test_app

--- test_project/test_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include
-from django.urls import re_path
-
-from django_url_security.tests import admin
-
-urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^grappelli/', include('grappelli.urls')),
-]
