25f939e7c49ed63ab1e366110db7ae80b73184d1
Matt Fisher <m@ttfisher.com>
2024-07-22 17:06:06 +1000

    Cope with missing default fixture module

--- django_url_security/fixtures.py
+++ django_url_security/fixtures.py
@@ -9,4 +9,15 @@ Once this is imported, you can access the fixtures defined in that file
 by including the name of the fixture factory functions in the URL security spec file
 e.g. 'poll_question' in the example_project/url_security_fixtures.py file
 """
-fixture_module = import_module(DEFAULT_FIXTURE_MODULE)
+# TODO: Allow manually specifying the fixture module
+fixture_module_name = DEFAULT_FIXTURE_MODULE
+
+try:
+    fixture_module = import_module(fixture_module_name)
+except ImportError:
+    if fixture_module_name != DEFAULT_FIXTURE_MODULE:
+        # If the fixture module has been specified and isn't there, raise an error
+        raise
+    else:
+        # If the fixture module is the default but isn't there, ignore it.
+        fixture_module = None
