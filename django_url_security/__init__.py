4e23233fcd7798d9b5aabe526bc76ec676a49c66
Matt Fisher <m@ttfisher.com>
2024-08-03 17:29:11 +1000

    Ruff

--- django_url_security/__init__.py
+++ django_url_security/__init__.py
@@ -5,7 +5,7 @@ from importlib_metadata import PackageNotFoundError, version
 __author__ = 'Matt Fisher'
 __email__ = 'm@ttfisher.com'
 
-# Used to automatically set version number from github actions
+# Used to automatically set version number from GitHub actions
 # as well as not break when being tested locally
 try:
     __version__ = version(__package__)  # type: ignore
